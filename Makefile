326199615c3842d5c68686adfc96aa03e013e9b0
Brian Mock <brian@mockbrian.com>
2015-07-15 11:40:44 -0700

    Initial support for command line compiler

--- Makefile
+++ Makefile
@@ -1,17 +1,16 @@
 all: build run
 
-FILES  = grammars/* examples/* src/* runtime/*
+FILES = grammars/* examples/* src/* runtime/* Makefile
 
 watch:
 	ls $(FILES) | entr -rc make run
 
 build:
 	npm run travis
-	jison grammars/parser.jison -o build/parser.js
-	# pegjs --cache parser.pegjs
-	# pegjs parser.pegjs
+	[ -d build ] || mkdir build
+	npm run jison -- grammars/parser.jison -o build/parser.js
 
 run: build
-	node src/main.js
+	node src/main.js examples/input.txt
 
 .PHONY: watch build run all
--- src/main.js
+++ src/main.js
@@ -1,40 +1,56 @@
+#!/usr/bin/env node
 "use strict";
 
+var pkg = require("../package.json");
 var fs = require("fs");
+var path = require("path");
+var argv = require("yargs")
+    .demand(1)
+    .usage("Usage: squiggle [options] [file]")
+    .option("output", {
+        alias: "o",
+        describe: "Write JavaScript to this file",
+        nargs: 1,
+        string: true,
+        demand: true
+    })
+    .option("help", {
+        alias: "h",
+        describe: "Print this message"
+    })
+    .option("main", {
+        alias: "m",
+        describe: "Compile as main file without module.exports",
+    })
+    .version(pkg.version)
+    .alias("version", "v")
+    .showHelpOnFail(true)
+    .strict()
+    .argv;
+
+console.log(argv);
+
 var parse = require("./parse");
 var transformAst = require("./transform-ast");
 var compile = require("./compile");
 var lint = require("./lint");
-var path = require("path");
-var f = path.join(__dirname, "../examples/input.txt");
-var txt = fs.readFileSync(f, "utf-8");
 
+var UTF8 = "utf-8";
+
+function die(message) {
+    console.error("squiggle: error: " + message);
+    process.exit(1);
+}
+
+var txt = fs.readFileSync(argv._[0], "utf-8");
 var ast = parse(txt);
 var warnings = lint(ast);
+
 warnings.forEach(function(m) {
-    console.error('lint: ' + m);
+    console.error('squiggle: lint: ' + m);
 });
+
 var es = transformAst(ast);
 var code = compile(es);
-var json = JSON.stringify(ast, null, 2);
 
-// console.log("=== AST ===");
-// console.log(json);
-
-// console.log("=== CODE ===");
-// console.log(code);
-
-function unsafeEval(code) {
-    /*jshint evil:true*/
-    return eval(code);
-}
-
-var theModule = unsafeEval(code);
-
-console.log("=== MODULE ===");
-console.log(theModule);
-
-if (theModule && typeof theModule.main === 'function') {
-    console.log("=== MAIN ===");
-    console.log(theModule.main());
-}
+fs.writeFileSync(argv.output, code, UTF8);
