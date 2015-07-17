9460ce93501ab4a12f397eb9b367d9ac00c82011
Brian Mock <brian@mockbrian.com>
2015-07-16 17:42:21 -0700

    Slight cleanup

--- src/main.js
+++ src/main.js
@@ -1,25 +1,31 @@
 #!/usr/bin/env node
 "use strict";
 
+var usage = [
+    "Usage: squiggle [file] -o [file]",
+    "",
+    "Examples: squiggle main.squiggle -o main.js"
+].join("\n");
+
 var pkg = require("../package.json");
 var fs = require("fs");
 var path = require("path");
 var argv = require("yargs")
     .demand(1)
-    .usage("Usage: squiggle [options] [file] --output [file]")
-    .option("output", {
-        alias: "o",
-        describe: "Write JavaScript to this file",
+    .usage(usage)
+    .option("o", {
+        alias: "output",
+        describe: "Write JavaScript to this file or directory",
         nargs: 1,
         string: true,
         demand: true
     })
-    .option("help", {
-        alias: "h",
+    .option("h", {
+        alias: "help",
         describe: "Print this message"
     })
+    .alias("v", "version")
     .version(pkg.version)
-    .alias("version", "v")
     .epilogue("Version " + pkg.version)
     .showHelpOnFail(true)
     .strict()
@@ -37,15 +43,16 @@ function die(message) {
     process.exit(1);
 }
 
-var txt = fs.readFileSync(argv._[0], "utf-8");
-var ast = parse(txt);
-var warnings = lint(ast);
+function compileTo(src, dest) {
+    var txt = fs.readFileSync(src, "utf-8");
+    var ast = parse(txt);
+    var warnings = lint(ast);
+    warnings.forEach(function(m) {
+        console.error('squiggle: lint: ' + m);
+    });
+    var es = transformAst(ast);
+    var code = compile(es);
+    fs.writeFileSync(dest, code, UTF8);
+}
 
-warnings.forEach(function(m) {
-    console.error('squiggle: lint: ' + m);
-});
-
-var es = transformAst(ast);
-var code = compile(es);
-
-fs.writeFileSync(argv.output, code, UTF8);
+compileTo(argv._[0], argv.output);
