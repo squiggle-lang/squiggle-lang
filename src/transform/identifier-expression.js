fc0f6c015404b3acd6faa7cc65dab3170c87f7f7
Dylan Scott <drrscott@hotmail.com>
2015-10-03 13:49:41 -0500

    Updated identifier-expression.js for issue #58
    Added die on reference being '_'

--- src/transform/identifier-expression.js
+++ src/transform/identifier-expression.js
@@ -1,9 +1,13 @@
+var die = require("../die");
 var es = require("../es");
 
 function IdentifierExpression(transform, node) {
     var id = transform(node.data);
     var name = es.Literal(node.data.data);
     var ref = es.Identifier("$ref");
+    if (ref === "_") {
+        die("squiggle: cannot compile a reference to a variable named _ as that will never be bound");
+    }
     return es.CallExpression(ref, [id, name]);
 }
 
