5c39f3a7c25020fa8de057cf828232678b2d29a2
Dylan Scott <drrscott@hotmail.com>
2015-10-04 14:15:02 -0500

    Updated to use correct variable to check
    Changed from ref check for '_' to name.value

--- src/transform/identifier-expression.js
+++ src/transform/identifier-expression.js
@@ -4,11 +4,11 @@ var es = require("../es");
 function IdentifierExpression(transform, node) {
     var id = transform(node.data);
     var name = es.Literal(node.data.data);
-    var ref = es.Identifier("$ref");
-    if (ref === "_") {
+    if (name.value === "_") {
         die("squiggle: variable '_' cannot be referenced " +
             "as it will never be bound");
     }
+    var ref = es.Identifier("$ref");
     return es.CallExpression(ref, [id, name]);
 }
 
