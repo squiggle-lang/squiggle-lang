b2c0b4b3b3c614809f2be780939bdc72306d6ca9
Dylan Scott <drrscott@hotmail.com>
2015-10-04 23:23:39 -0500

    Reverted change to exit
    Making it always exit on die

--- src/die.js
+++ src/die.js
@@ -6,9 +6,7 @@ var ERROR = chalk.bold.red;
 function die() {
     var msg = [].join.call(arguments, " ");
     console.error(ERROR(msg));
-    if (argv !== undefined && !argv.interactive) {
-        process.exit(1);
- 	}
+    process.exit(1);
 }
 
 module.exports = die;
