5246143a70e5344a447a19baf691535aa1efab06
Dylan Scott <drrscott@hotmail.com>
2015-10-04 23:22:49 -0500

    Updated to throw exception per PR discussion
    Changed to throw Error instead of die per PR discussion

--- src/transform/identifier-expression.js
+++ src/transform/identifier-expression.js
@@ -1,11 +1,10 @@
-var die = require("../die");
 var es = require("../es");
 
 function IdentifierExpression(transform, node) {
     var id = transform(node.data);
     var name = es.Literal(node.data.data);
     if (name.value === "_") {
-        die("squiggle: variable '_' cannot be referenced " +
+        throw new Error("squiggle: variable '_' cannot be referenced " +
             "as it will never be bound");
     }
     var ref = es.Identifier("$ref");
