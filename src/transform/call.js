cb4d7b23b5b537568bf168b28905ddc5ffb906f8
Brian Mock <brian@mockbrian.com>
2015-10-05 23:03:33 -0700

    Adds comment

--- src/transform/call.js
+++ src/transform/call.js
@@ -2,6 +2,10 @@ var es = require("../es");
 var ast = require("../ast");
 
 function Call(transform, node) {
+    // Id the function being called comes from a "GetProperty", that means we're
+    // looking at a method invocation, like `foo.bar()` rather than just a
+    // normal function invocation like `goop()`, so transform this node into a
+    // `ast.CallMethod` and let it do the work.
     if (node.f.type === "GetProperty") {
         var obj = node.f.obj;
         var prop = node.f.prop;
