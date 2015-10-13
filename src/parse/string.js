54d5756085f07e25d8506b1910cd1697dc61e0b9
Brian Mock <brian@mockbrian.com>
2015-10-13 12:44:19 -0700

    Fixes #69 improves string literals

--- src/parse/string.js
+++ src/parse/string.js
@@ -1,13 +1,38 @@
 var P = require("parsimmon");
 
 var ast = require("../ast");
+var H = require("../parse-helpers");
 
-var DQuote = P.string('"');
+var join = H.join;
+var wrap = H.wrap;
 
-var StringChars = P.regex(/[^"\n]*/);
+var Char = P.regex(/[^"\n\\]+/);
+
+var SimpleEscape =
+    P.alt(
+        P.string("\\\\").result("\\"),
+        P.string("\\\"").result("\""),
+        P.string("\\n").result("\n"),
+        P.string("\\t").result("\t"),
+        P.string("\\r").result("\r")
+    );
+
+var HexNumber =
+    P.regex(/[0-9a-fA-F]+/)
+    .map(function(s) { return Number("0x" + s); });
+
+// TODO: Polyfill String.fromCodePoint so this works in older enviornments.
+var UnicodeEscape =
+    wrap("\\u{", HexNumber, "}")
+    .map(String.fromCodePoint)
+
+var StringInner =
+    P.alt(SimpleEscape, UnicodeEscape, Char)
+    .many()
+    .map(join(""));
 
 var String_ =
-    DQuote.then(StringChars).skip(DQuote)
+    wrap('"', StringInner, '"')
     .desc("string")
     .map(ast.String);
 
