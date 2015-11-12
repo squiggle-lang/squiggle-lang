374a3c2afad39406e00315ffa015276816f7c97d
noah morrison <noah@morrison.ph>
2015-11-12 17:00:02 -0500

    Generate literals for negative numbers
    As opposed to $negate(4) we now do -4
    
    Closes #66

--- src/transform/negate.js
+++ src/transform/negate.js
@@ -2,8 +2,13 @@ var es = require("../es");
 
 function Negate(transform, node) {
     var expr = transform(node.expr);
-    var negate = es.Identifier(node.loc, "$negate");
-    return es.CallExpression(null, negate, [expr]);
+    if (typeof node.expr.data === "number") {
+        var literal = es.Literal(node.loc, node.expr.data);
+        return es.UnaryExpression(node.loc, true, "-", literal);
+    } else {
+        var negate = es.Identifier(node.loc, "$negate");
+        return es.CallExpression(null, negate, [expr]);
+    }
 }
 
 module.exports = Negate;
