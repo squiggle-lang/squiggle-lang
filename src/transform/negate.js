2885aa520808d944b76387bc4f2dc8c9896c6dcf
noah morrison <noah@morrison.ph>
2015-11-12 17:39:08 -0500

    Use proper type checking and transform function
    As per Brians requests

--- src/transform/negate.js
+++ src/transform/negate.js
@@ -2,9 +2,8 @@ var es = require("../es");
 
 function Negate(transform, node) {
     var expr = transform(node.expr);
-    if (typeof node.expr.data === "number") {
-        var literal = es.Literal(node.loc, node.expr.data);
-        return es.UnaryExpression(node.loc, true, "-", literal);
+    if (node.expr.type === "Number") {
+        return es.UnaryExpression(node.loc, true, "-", transform(node.expr));
     } else {
         var negate = es.Identifier(node.loc, "$negate");
         return es.CallExpression(null, negate, [expr]);
