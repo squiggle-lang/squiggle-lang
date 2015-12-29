6836dad68ad04c99d505e00e2e4efab1f921ebb4
Brian Mock <brian@mockbrian.com>
2015-12-28 19:10:26 -0800

    Fixes #135 adds 'else' sugar to 'match'

--- src/parse/match.js
+++ src/parse/match.js
@@ -7,16 +7,25 @@ var _ = require("./whitespace")(null);
 var ast = require("../ast");
 
 var word = H.word;
+var ione = H.ione;
 var iseq = H.iseq;
 
 module.exports = function(ps) {
+    var emptyPattern =
+        ione(ast.PatternSimple,
+            ione(ast.Identifier, P.of("_")));
+    var NormalClause =
+        P.seq(
+            word("case").then(ps.MatchPattern),
+            _.then(word("then")).then(ps.Block)
+        );
+    var ElseClause =
+        P.seq(
+            emptyPattern,
+            _.then(word("else")).then(ps.Block)
+        );
     var MatchClause =
-        iseq(ast.MatchClause,
-            P.seq(
-                word("case").then(ps.MatchPattern),
-                _.then(word("then")).then(ps.Block)
-            ));
-
+        iseq(ast.MatchClause, NormalClause.or(ElseClause));
     return iseq(ast.Match,
         P.seq(
             word("match").then(ps.BinExpr),
