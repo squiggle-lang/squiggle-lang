afa3712a44f7eb29b0780d3a0d2952c26ac1f9d4
Brian Mock <brian@mockbrian.com>
2016-01-26 22:27:28 -0800

    Fixes parse bug in rare case
    Example like this would cause a parse issue: do let x=x;)

--- src/parse/block.js
+++ src/parse/block.js
@@ -7,16 +7,22 @@ var H = require("../parse-helpers");
 
 var iseq = H.iseq;
 
+function checkBlock(statements) {
+    var n = statements.length;
+    var i = n - 1;
+    var last = statements[i];
+    if (last.type !== "ExprStmt") {
+        return P.fail("blocks must end with an expression");
+    } else {
+        return P.of([statements.slice(0, i), last]);
+    }
+}
+
 module.exports = function(ps) {
     return iseq(ast.Block,
-        ps.Statement.skip(ps.Terminator).atLeast(1).map(function(statements) {
-            var n = statements.length;
-            var i = n - 1;
-            var last = statements[i];
-            if (last.type !== "ExprStmt") {
-                return P.fail("blocks must end with an expression");
-            }
-            return [statements.slice(0, i), last];
-        })
+        ps.Statement
+            .skip(ps.Terminator)
+            .atLeast(1)
+            .chain(checkBlock)
     );
 };
