296fe5c92f736e19fc7723a1cb96577cf5fb1435
Brian Mock <brian@mockbrian.com>
2016-03-27 20:24:28 -0700

    Fixes build

--- src/rt/regexp.js
+++ src/rt/regexp.js
@@ -1,23 +1,22 @@
 function replaceAll() {
-  throw new Error("not implemented")
+  throw new Error("not implemented");
 }
 
 function replaceAllWith() {
-  throw new Error("not implemented")
+  throw new Error("not implemented");
 }
 
 function match() {
-  throw new Error("not implemented")
+  throw new Error("not implemented");
 }
 
 function test() {
-  throw new Error("not implemented")
+  throw new Error("not implemented");
 }
 
 function split() {
-  throw new Error("not implemented")
+  throw new Error("not implemented");
 }
-
 
 exports.replaceAll = replaceAll;
 exports.replaceAllWith = replaceAllWith;
